b5357eabfe5fa16177f494b9bfa0cf2d1bdf71a0
myinan <m.yasir.inan@gmail.com>
2024-02-07 17:47:01 +0300

    Create logic for computer to try adjacent cells on hit

--- src/modules/createPlayer.js
+++ src/modules/createPlayer.js
@@ -48,8 +48,17 @@ function coordinatesAlreadyAttacked(coords) {
   );
 }
 
-// Function for computer's play
-computerPlayer.play = function play(enemyBoard) {
+// Variable to keep track of latest attack cell
+let latestAttackedCell = {
+  cellValue: null,
+  hitCoor: null,
+};
+
+// Variable to keep track of adjacent ship cells on ship hit
+const shipCellsCoords = [];
+
+// Helper function for random attacks
+function attackRandom(enemyBoard) {
   let hitCoor;
 
   // Keep generating random coordinates until a not-attacked one is found
@@ -65,6 +74,82 @@ computerPlayer.play = function play(enemyBoard) {
   // Perform the attack on the enemy board
   const cellValue = computerPlayer.attack(enemyBoard, hitCoor);
   return { hitCoor, cellValue };
+}
+
+// Helper function to attack previous or next cells of an already hit ship
+function attackPrevNext(enemyBoard, hitCoor) {
+  computerPlayer.attackedCoords.push(hitCoor);
+  const cellValue = computerPlayer.attack(enemyBoard, hitCoor);
+  return { hitCoor, cellValue };
+}
+
+// Function for computer's play
+computerPlayer.play = function play(enemyBoard) {
+  if (
+    latestAttackedCell.cellValue === null ||
+    latestAttackedCell.cellValue === "0" ||
+    shipCellsCoords.length === 0
+  ) {
+    latestAttackedCell = attackRandom(enemyBoard);
+    // This mean a ship got hit
+    if (latestAttackedCell.cellValue !== "0") {
+      // Store references to prev and next cells
+      const { row } = latestAttackedCell.hitCoor;
+      const { column } = latestAttackedCell.hitCoor;
+      let columnNext = column;
+      let columnPrev = column;
+      while (
+        enemyBoard.board[row][columnNext + 1] === latestAttackedCell.cellValue
+      ) {
+        columnNext += 1;
+        shipCellsCoords.push({ row, column: columnNext });
+      }
+
+      while (
+        enemyBoard.board[row][columnPrev - 1] === latestAttackedCell.cellValue
+      ) {
+        columnPrev -= 1;
+        shipCellsCoords.push({ row, column: columnPrev });
+      }
+    }
+    return latestAttackedCell;
+  }
+  if (shipCellsCoords.length !== 0) {
+    latestAttackedCell = attackPrevNext(enemyBoard, shipCellsCoords[0]);
+    shipCellsCoords.shift();
+  }
+  return latestAttackedCell;
 };
 
 export { humanPlayer, computerPlayer };
+
+// Pseuodocode
+/* 
+let latestAttackedCell;
+let shipCellsCoords;
+
+function attackRandom() {}
+function attackPrevNext() {}
+
+computerPlayer.play = function play(enemyBoard) {
+  If (latestAttackedCell === "0") { 
+    latestAttackedCell = attackRandom()
+    If ( latestAttackedCell !== "0") { 
+      // Store references to prev and next cells 
+      Update shipCellsCoords
+    }
+    return latestAttackedCell
+  }
+  
+  if ( shipCellsCoords.length !== 0) {
+    // Keep attacking shipCellsCoords until store Arr is empty
+    attackPrevNext(shipCellsCoords[0])
+    shipCellsCoords.shift()
+    if (shipCellsCoords.length === 0) {
+      // If store Arr is empty, update latestAttackedCell to "0"
+      latestAttackedCell = "0";
+    }
+    return ?
+  }
+} 
+*/
